b62be4159e16e61b6ccfc3f40b7f70e44cca871c
Lucca Paes <152209319+Luccapaco@users.noreply.github.com>
2024-10-10 04:03:36 -0300

    agora tem o sub, está em string
    tem o 0(sinal) e o 11 ( ' \0 ' ) porem ainda nao funciona com numero negativo

--- somabin.c
+++ somabin.c
@@ -1,63 +1,54 @@
 #include <stdio.h>
+#include <string.h>
 
 //TA FUNCIONANDO  A = BINARIO 1  B = BINARIO 2  C = RESULTADO
-void soma(int a[], int b[], int c[]){
-    int extra;
-    for(int i = 4; i > 0; i--){
-        printf("i = %d\n", i);
-        if(a[i] == 1 && b[i] == 0  && extra == 0 || a[i] == 0 && b[i] == 1 && extra == 0){
-            c[i] = 1;
-            printf("a\n");
+void soma(char a[], char b[], char c[]){
+    int extra = 0;
+    printf("somando %s e %s\n", a, b);
+    for(int i = 10; i > 0; i--){
+        if((a[i] == '1' && b[i] == '0'  && extra == 0) || (a[i] == '0' && b[i] == '1' && extra == 0)){
+            c[i] = '1';
         }
-        else if(a[i] == 1 && b[i] == 1 && extra == 0){
-            c[i] = 0;
+        else if(a[i] == '1' && b[i] == '1' && extra == 0){
+            c[i] = '0';
             extra = 1;
-            printf("b\n");
         }
-        else if(a[i] == 0 && b[i] == 0 && extra == 0){
-            c[i] = 0;
-            printf("c\n");
+        else if(a[i] == '0' && b[i] == '0' && extra == 0){
+            c[i] = '0';
         }
-        else if(a[i] == 1 && b[i] == 0  && extra == 1 || a[i] == 0 && b[i] == 1 && extra == 1){
-            c[i] = 0;
+        else if((a[i] == '1' && b[i] == '0'  && extra == 1) || (a[i] == '0' && b[i] == '1' && extra == 1)){
+            c[i] = '0';
             extra = 1;
-            printf("d\n");
         }
-        else if(a[i] == 1 && b[i] == 1  && extra == 1){
-            c[i] = 1;
+        else if(a[i] == '1' && b[i] == '1'  && extra == 1){
+            c[i] = '1';
             extra = 1;
-            printf("e\n");
         }
-        else if(a[i] == 0 && b[i] == 0 && extra == 1){
-            c[i] = 1;
+        else if(a[i] == '0' && b[i] == '0' && extra == 1){
+            c[i] = '1';
             extra = 0;
-            printf("f\n");
         }
-        else{
-            printf("pqp\n");
-        }
+        printf("c[%d] = %c\n", i,c[i]);
     }
-    printf("Resultado da soma = ");
-    for(int i = 0; i < 5; i++){
-        printf("%d", c[i]);
-    }
+    printf("resultado = %s \n", c);
     return;
 }
 
-//AGORA FUNCIONA
-void sub(int a[], int b[], int c[]){
-        for(int i = 4; i > 0; i--){
-            if(b[i] == 0){
-                b[i] = 1;
+void sub(char a[], char b[], char c[]){
+        for(int i = 10; i > 0; i--){
+            if(b[i] == '0'){
+                b[i] = '1';
             }
             else{
-                b[i] = 0;
+                b[i] = '0';
             }
         }
-        int aux[5] = {0,0,0,0,1};
+        printf("b depois da inversão = %s\n", b);
+        char aux[12] = {'0','0','0','0','0','0','0','0','0','0','1','\0'};
         soma(b,aux,c);
+        printf("voltei da primeira soma\n");
 
-        for(int i = 1; i < 5; i++){
+        for(int i = 11; i > 0; i--){
             b[i] = c[i];
         }
 
@@ -65,7 +56,7 @@ void sub(int a[], int b[], int c[]){
 }
 
 //pra ver se é soma subtração esses caralho mas ta incompleto so funciona de soma
-void operacao(int a[], int b[], int c[], char sinal){
+void operacao(char a[], char b[], char c[], char sinal){
     switch(sinal){
         case '+': soma(a, b, c); break;
         case '-': sub(a, b, c); break;
@@ -74,41 +65,28 @@ void operacao(int a[], int b[], int c[], char sinal){
 }
 
 int main(){
-    int a[5];
-    int b[5];
-    int c[5] = {0};
+    char a[12] = {'\0'};
+    char b[12] = {'\0'};
+    char c[12] = {'\0'};
     char sinal;
 
     //pra pegar os numeros mas a gente vai fazer de string
-    printf("Digite o sinal do primeiro binário (0 = + // 1 = -): \n");
-    scanf("%d", &a[0]);
-    printf("Digite o primeiro binário: \n");
-    for(int i = 1; i < 5; i++){
-        scanf("%d", &a[i]);
-    }
+    printf("Digite o primeiro binário(11 dígitos) com o primeiro sendo o sinal (0 = + // 1 = -): \n");
+    scanf("%11s", a);
 
-    printf("Digite o sinal do segundo binário (0 = + // 1 = -): \n");
-    scanf("%d", &b[0]);
-    printf("Digite o segundo binário: \n");
-    for(int i = 1; i < 5; i++){
-        scanf("%d", &b[i]);
-    }
+    printf("Digite o segundo binário(11 dígitos) com o primeiro sendo o sinal (0 = + // 1 = -): \n");
+    scanf("%11s", b);
 
     printf("Digite o sinal: \n");
     scanf(" %c", &sinal);
     
-    for(int i = 0; i < 5; i++){
-        printf("%d", a[i]);
-    }
-    printf("\n");
-    for(int i = 0; i < 5; i++){
-        printf("%d", b[i]);
-    }
-    printf("\n");
+    printf("a = %s \n", a);
+    printf("b = %s \n", b);
+
 
     //pra ver os sinais
     if(b[0] != a[0]){
-        for(int i = 1; i < 5; i++){
+        for(int i = 1; i < 11; i++){
             //isso aqui ve quem é maior
             if(a[i] > b[i]){
                 c[0] = a[0];
@@ -120,7 +98,7 @@ int main(){
                 operacao(b, a, c, sinal);
                 break;
             }
-            else if(i == 4 && a[i] == b[i]){
+            else if(i == 10 && a[i] == b[i]){
                 printf("Resultado = 00000\n");
                 return 0;
             }
