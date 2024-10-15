5a74532564a702c8bcc0c382a848f99f156254f9
André <139287902+pxdroAndre@users.noreply.github.com>
2024-10-14 23:57:22 -0300

    Update main.c

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -17,10 +17,10 @@ int main()
   empilhar_num(entrada, &pilha_num, &pilha_op, 0, 0, 0);
   // printf("b1: %s op:%c b2: %s \n", pilha_num.strings[0], pilha_op.characters[pilha_op.topo], pilha_num.strings[1]);
   // imprimir_bins(pilha_num);
-  imprimir_ops(pilha_op);
+  // imprimir_ops(pilha_op);
   for (int i = pilha_op.topo; pilha_op.topo != -1; i--)
   {
-    printf("i:%d\n", i);
+    // printf("i:%d\n", i);
     desempilha_e_opera(&pilha_num, &pilha_op);
   }
   printf("Resultado: %s\n", pilha_num.strings[pilha_num.topo]);
