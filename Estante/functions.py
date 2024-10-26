92ca3d9d1ce471c9ee742d69f21cce56ca45dbe0
André <139287902+pxdroAndre@users.noreply.github.com>
2024-10-26 00:34:13 -0300

    Delete Estante/functions.py

--- Estante/functions.py
+++
@@ -1,6 +0,0 @@
-#funcao que retorna o campo de um array, mas caso o campo nao exista, retorna None
-def get_field(fields, index):
-    if len(fields) > index:
-        return fields[index]
-    else:
-        return None
