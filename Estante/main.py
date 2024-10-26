1c97f239ff4670537a2377fd71c2bef119d9b09a
André <139287902+pxdroAndre@users.noreply.github.com>
2024-10-26 00:34:47 -0300

    Update main.py

--- Estante/main.py
+++ Estante/main.py
@@ -1,78 +1,65 @@
-from functions import get_field
-from classes import Biblioteca
+from classes import Livro, Biblioteca
 
-def criar_biblioteca():
-    # receber o input
-    with open('./entrada.txt', 'r') as arquivo:
-        linhas = arquivo.readlines()
-
+#funcao para ler os livros do arquivo
+def ler_livros_arquivo(arquivo_path):
+    # lista para armazenar os livros
     livros = []
-    for linha in linhas:
-        linha = linha.strip()
-        # se a linha não for vazia
-        if linha:
-            # separar os dados
-            partes = linha.split(',')
-            # pegar os dados
-            titulo = partes[0].split('=')[1].strip()
-            autor = partes[1].split('=')[1].strip()
-            largura = int(partes[2].split('=')[1].strip())
-            altura = int(partes[3].split('=')[1].strip())
-            profundidade = int(partes[4].split('=')[1].strip())
-            # adicionar os dados na lista de livros
-            livros.append([titulo, largura, autor, altura, profundidade])
+    with open(arquivo_path, 'r') as arquivo: #abre o arquivo para ser lido
+        linhas = arquivo.readlines() #lê e armazena as linhas do arquivo
+        for i, linha in enumerate(linhas): #for para percorrer as linhas 
+            linha = linha.strip()#funcao para remover os espaços em branco
+            if linha:
+                #vai verificar se a linha não está vazia e separar os elementos da linha usando , como separador e = para separar o atributo do valor
+                partes = linha.split(',')
+                titulo = partes[0].split('=')[1].strip()
+                autor = partes[1].split('=')[1].strip()
+                largura = int(partes[2].split('=')[1].strip())
+                altura = int(partes[3].split('=')[1].strip())
+                profundidade = int(partes[4].split('=')[1].strip())
+                volume = altura * largura * profundidade
+                #depois de separar, cria um objeto livro com os valores 
+                livro = Livro(titulo, autor, largura, altura, profundidade, volume, i+1)
+                livros.append(livro) #adiciona o livro na lista de livros
+    return livros #retorna a lista de livros
 
-    # Ordenar os livros por altura e depois por largura (decrescente)
-    livros.sort(key=lambda livro: (livro[3], livro[1]), reverse=True)
-
-    # Criar as prateleiras
-    prateleiras = []
-    prateleira_atual = []
-    largura_atual = 0
-    largura_maxima_prateleira = 96
-
+#funcao que vai organizar a biblioteca
+def organizar_biblioteca(arquivo_path):  
+    livros = ler_livros_arquivo(arquivo_path)
+    
+    # ordenar os livros por altura e largura em ordem decrescente
+    livros.sort(key=lambda x: (x.volume))
+    
+    biblioteca = Biblioteca()
     
     for livro in livros:
-        largura_livro = livro[1]
-        # se a largura do livro for menor ou igual a largura máxima da prateleira add um livro na prateleira
-        if largura_atual + largura_livro <= largura_maxima_prateleira:
-            prateleira_atual.append(livro)
-            largura_atual += largura_livro
-        else:
-            prateleiras.append(prateleira_atual)
-            prateleira_atual = [livro]
-            largura_atual = largura_livro
-
-    if prateleira_atual:
-        prateleiras.append(prateleira_atual)
-
-    return Biblioteca(prateleiras)
-
-
-def sortar_biblioteca(biblioteca):
-    livros = []
-    for estante in biblioteca.estantes:
-        for prateleira in estante.prateleiras:
-            l = prateleira.limpar_prateleira()
-            for livro in l:
-                livros.append(livro)
-
-    livros = sorted(livros, key=lambda livro: (livro.genero, livro.autor, livro.ano, livro.titulo))
-
-    estante = 0
-    prateleira = 0
-    while len(livros) > 0:
-        livro = livros.pop(0)
-        adicionar = biblioteca.estantes[estante].prateleiras[prateleira].inserir_livro_existente(livro)
-        if adicionar == False:
-            prateleira += 1
-            if prateleira == 6:
-                estante += 1
-                prateleira = 0
-            adicionar = biblioteca.estantes[estante].prateleiras[prateleira].inserir_livro_existente(livro)
-
-
-# Criando e organizando a biblioteca
-my_library = criar_biblioteca()
-sortar_biblioteca(my_library)
-print(my_library)
+        biblioteca.adicionar_livro(livro)
+    return biblioteca
+    
+def menu(acao):
+    match acao:
+        case 1:
+            print(biblioteca)
+            return
+        case 2:
+            #funcao de consultar o livro
+            return
+        case 3:
+            #funcao de pegar emprestado
+            return
+        case 4:
+            #funcao de inserir livro
+            return
+        case 5:
+            #funcao de remover livro
+            return
+        case _:
+            print("acao invalida!\n")
+            return
+            
+arquivo_path = 'entrada.txt'
+biblioteca = organizar_biblioteca(arquivo_path)
+print("Seus livros foram armazenados!\nEscolha uma das opcoes a seguir\n")
+print("Digite um numero correspondente\n")
+print("[1] Imprimir a Biblioteca \n[2] Consultar um livro \n[3] Pegar emprestado \n[4] Inserir livro \n[5] Remover livro")
+acao=int(input())
+menu(acao)
