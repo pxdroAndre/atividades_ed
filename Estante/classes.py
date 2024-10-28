696e6f723ccdb00fdef6c0b2ce0a38aa57f7e923
André <139287902+pxdroAndre@users.noreply.github.com>
2024-10-28 15:49:13 -0300

    Update classes with remove function

--- Estante/classes.py
+++ Estante/classes.py
@@ -1,4 +1,4 @@
-#mesmo esquema do outro código, classes que são como structs e possuem uma hierarquia de organização
+# mesmo esquema do outro código, classes que são como structs e possuem uma hierarquia de organização
 
 class Livro:
     def __init__(self, titulo, autor, largura, altura, profundidade, volume, indice):
@@ -12,7 +12,7 @@ class Livro:
 
     def __str__(self):
         return f"Livro[{self.indice}]: {self.titulo} Autor: {self.autor} ({self.largura}cm x {self.altura}cm x {self.profundidade}cm x {self.volume}cm3)\n"
-    
+
 
 class Prateleira:
     def __init__(self, largura_maxima):
@@ -27,8 +27,17 @@ class Prateleira:
             return True
         return False
 
+    def remover_livro(self, indice):
+        for livro in self.livros:
+            if livro.indice == indice:
+                self.livros.remove(livro)
+                self.largura_usada -= livro.largura
+                return True
+        return False
+
     def __str__(self):
         return "\n".join(str(livro) for livro in self.livros)
+
 
 class Estante:
     def __init__(self, num_prateleiras=6, largura_maxima=96):
@@ -40,8 +49,14 @@ class Estante:
                 return True
         return False
 
+    def remover_livro(self, indice):
+        for prateleira in (self.prateleiras):
+            if prateleira.remover_livro(indice):
+                return True
+        return False
+
     def __str__(self):
-        return "\n".join([f"Prateleira {i+1}:\n{prateleira}\n" for i, prateleira in enumerate(self.prateleiras)])
+        return "\n".join([f"Prateleira {i + 1}:\n{prateleira}\n" for i, prateleira in enumerate(self.prateleiras)])
 
 
 class Biblioteca:
@@ -60,8 +75,15 @@ class Biblioteca:
         nova_estante.adicionar_livro(livro)
         self.estantes.append(nova_estante)
 
+    def remover_livro(self, indice):
+        for estante in (self.estantes):
+            if estante.remover_livro(indice):
+                return True
+        return False
+
     def __str__(self):
         result = []
         for i, estante in enumerate(self.estantes):
-            result.append(f"-------------------ESTANTE {i+1}:-------------------\n{estante}---------------------------------------------------------------\n")
+            result.append(
+                f"-------------------ESTANTE {i + 1}:-------------------\n{estante}---------------------------------------------------------------\n")
         return "\n".join(result)
